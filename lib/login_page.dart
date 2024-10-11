4da16d4a6682984ca23b3cf9f8dd23b54930185a
Jayesh <jayeshjain0975@gmail.com>
2024-10-11 13:46:36 +0530

    Added Login

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:cropcart/signup_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -8,9 +10,13 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  final emailController = TextEditingController(); 
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
+
+  Login() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailController.text, password: passwordController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,22 +27,21 @@ class _LoginPageState extends State<LoginPage> {
       body: Stack(
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.only(
+            borderRadius: const BorderRadius.only(
               bottomLeft: Radius.circular(30),
               bottomRight: Radius.circular(30),
             ),
             child: Image.asset(
               'assets/login-bg.jpg',
               width: screenWidth,
-              height: screenHeight * 0.5,
+              height: screenHeight * 0.4,
               fit: BoxFit.cover,
             ),
           ),
-          
           Center(
             child: Container(
               width: screenWidth * 0.85,
-              padding: EdgeInsets.all(20),
+              padding: const EdgeInsets.all(20),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
@@ -45,16 +50,20 @@ class _LoginPageState extends State<LoginPage> {
                     color: Colors.black.withOpacity(0.2),
                     blurRadius: 10,
                     spreadRadius: 5,
-                    offset: Offset(0, 5), // Creates shadow below the container
+                    offset: const Offset(
+                        0, 5), // Creates shadow below the container
                   ),
                 ],
               ),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Icon(Icons.shopping_bag,size: 50,),
+                  Image.asset(
+                    'assets/app-logo.png',
+                    width: screenWidth * 0.5,
+                  ),
                   // Login form fields and button
-                  Text(
+                  const Text(
                     'Sign In',
                     style: TextStyle(
                       fontSize: 28,
@@ -62,7 +71,9 @@ class _LoginPageState extends State<LoginPage> {
                       color: Colors.black,
                     ),
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(
+                    height: 15,
+                  ),
 
                   // Email TextField
                   TextFormField(
@@ -72,26 +83,32 @@ class _LoginPageState extends State<LoginPage> {
                       fillColor: const Color(0xFFedf0f8),
                       filled: true,
                       hintText: 'Email',
-                      hintStyle: const TextStyle(color: Colors.grey),
+                      hintStyle: const TextStyle(
+                        color: Colors.grey,
+                      ),
                       focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
+                        borderSide: const BorderSide(color: Colors.orange),
                       ),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
+                        borderSide: const BorderSide(color: Colors.grey),
                       ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 9, horizontal: 15), // Adjust padding here
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
                         return 'Please enter your email';
-                      } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}').hasMatch(value)) {
+                      } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w]{2,4}')
+                          .hasMatch(value)) {
                         return 'Please enter a valid email';
                       }
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+
+                  const SizedBox(height: 15),
 
                   // Password TextField
                   TextFormField(
@@ -104,13 +121,14 @@ class _LoginPageState extends State<LoginPage> {
                       hintText: 'Password',
                       hintStyle: const TextStyle(color: Colors.grey),
                       focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
-                      ),
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: const BorderSide(color: Colors.orange)),
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none,
+                        borderSide: const BorderSide(color: Colors.grey),
                       ),
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 9, horizontal: 15),
                     ),
                     validator: (value) {
                       if (value == null || value.isEmpty) {
@@ -119,7 +137,7 @@ class _LoginPageState extends State<LoginPage> {
                       return null;
                     },
                   ),
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
 
                   // Login Button
                   ElevatedButton(
@@ -127,33 +145,64 @@ class _LoginPageState extends State<LoginPage> {
                       backgroundColor: Colors.orange, // Background color
                       minimumSize: Size(screenWidth * 0.7, 50), // Button size
                     ),
-                    onPressed: () {
-                      // Implement login functionality here
+                    onPressed: () async {
+                      await Login();
                     },
-                    child: Text('Login', style: TextStyle(fontSize: 18,color: Colors.white)),
+                    child: const Text('Login',
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
                   ),
+                  const SizedBox(height: 10,),
+                  GestureDetector(child: const Text('Forgot Password',style: TextStyle(color: Colors.grey),),),
 
-                  
-                  const SizedBox(height: 50),
+                  const SizedBox(height: 20),
                   const Text(
                     'OR Login with',
                     style: TextStyle(fontSize: 17),
                   ),
-                  const SizedBox(height: 30.0),
+                  const SizedBox(height: 20),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Image.asset(
                         "assets/google.png",
-                        height: 40,
+                        height: 35,
                         width: 45,
                       ),
                       const SizedBox(width: 30.0),
                       Image.asset(
                         "assets/apple.png",
-                        height: 50,
+                        height: 45,
                         width: 50,
                       ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 30.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'New user? ',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.w400),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const SignupPage()),
+                          );
+                        },
+                        child: const Text(
+                          'Register',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.orange),
+                        ),
+                      )
                     ],
                   ),
                 ],
@@ -165,4 +214,3 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 }
-
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,31 @@
-import 'package:cropcart/home_page.dart';
-import 'package:cropcart/login_page.dart';
+import 'dart:io';
+import 'package:cropcart/wrapper.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyDE5nrPaBXssFZ7bEPy6FtpKVl2Zjey8S0",
+            appId: "1:1037467007539:android:189d5273d00160f472c146",
+            messagingSenderId: "1037467007539",
+            projectId: "crop-cart",
+          ),
+        )
+      : Firebase.initializeApp();
+  Platform.isIOS
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: 'AIzaSyCaoSmxpbUwqshsLmxjeHSaA9iRAPZMejs',
+            appId: '1:1037467007539:ios:7d98d5090293d30a72c146',
+            messagingSenderId: '1037467007539',
+            projectId: 'crop-cart',
+          ),
+        )
+      : Firebase.initializeApp();
   runApp(const MyApp());
 }
  class MyApp extends StatelessWidget {
@@ -10,9 +33,9 @@ void main() {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  LoginPage(),
+      home:  Wrapper(),
     );
   }
 }
